fae9c9ce3aaf59f5c08a43952395606247bb0033
KashifUmar123 <52071155+KashifUmar123@users.noreply.github.com>
2023-05-11 17:07:08 +0500

    code update

--- config/amazonConfig.js
+++ config/amazonConfig.js
@@ -1,6 +1,6 @@
 const AWS = require("aws-sdk");
-const ACCESS_KEY = "AKIAQ56376SBIG3L4CWB";
-const SECRET_KEY = "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";
+const ACCESS_KEY = "fill";
+const SECRET_KEY = "fill";
 
 const s3 = new AWS.S3({
     accessKeyId: ACCESS_KEY,
